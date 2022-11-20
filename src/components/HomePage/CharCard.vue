0baf67ae81afc16de5724284a4c2a3d6036272d8
amrmugl <meet.sagi.here@gmail.com>
2022-11-21 04:11:21 +0500

    app done

---
+++ src/components/HomePage/CharCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-3 rounded bg-appLightBlue w-[210px]">
+    <div v-if="ch.image">
+      <img
+        class="h-[250px] w-[200px] bg-cover"
+        :src="ch.image"
+        :alt="ch.name"
+      />
+    </div>
+    <div class="h-[250px] w-[180px] bg-cover" v-else>No Image</div>
+    <h4 class="mt-4 font-bold">
+      Name: <span class="font-normal">{{ ch.name ? ch.name : "Unknown" }}</span>
+    </h4>
+    <p class="font-bold">
+      Actor:
+      <span class="font-normal">{{ ch.actor ? ch.actor : "Unknown" }}</span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Hogwarts } from "@/types/hogwarts";
+
+defineProps<{
+  ch: Hogwarts;
+}>();
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,9 +7,24 @@
     <nav>
       <OptionsList />
     </nav>
+    <main class="flex flex-row flex-wrap items-center mx-5 text-xs mt-14">
+      <div
+        class="m-1 text-xs text-center text-black"
+        v-for="ch in getCharacters"
+        :key="ch.name"
+      >
+        <CharCard :ch="ch" />
+      </div>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import CharCard from "@/components/HomePage/CharCard.vue";
 import OptionsList from "@/components/HomePage/OptionsList.vue";
+import { useHogwartsStore } from "@/stores/hogwarts";
+import { storeToRefs } from "pinia";
+
+const hogwartsStore = useHogwartsStore();
+const { getCharacters } = storeToRefs(hogwartsStore);
 </script>
